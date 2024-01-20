9befa789cf08d666d757aa9e459cbaab070ed35a
Abrham007 <abrhamaraya111@gmail.com>
2024-01-20 16:33:51 +0300

    started the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,47 @@
-import logoLight from "./assets/logo-light.svg";
+import Button from "./Button";
+import styled from "styled-components";
+import logoLight from "../assets/logo-light.svg";
+
+const StyledHeader = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 32px 0px 24px;
+
+  &.Header-text {
+    display: flex;
+    gap: 65px;
+  }
+
+  &.Header-btns {
+    display: flex;
+    gap: 24px;
+  }
+
+  & h2 {
+    color: #000112;
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+`;
 
 export default function Header() {
   return (
-    <header className="Header">
-      <h1 className="Header-logo">
-        <img src={logoLight}></img>
-        <span>kanban</span>
-      </h1>
-      <h2 className="Header-text">Platform Launch</h2>
-      <button></button>
-      <button></button>
-    </header>
+    <StyledHeader>
+      <div className="Header-text">
+        <h1>
+          <div role="presentation"></div>
+          <span>kanban</span>
+        </h1>
+        <hr></hr>
+        <h2>Platform Launch</h2>
+      </div>
+
+      <div className="Header-btns">
+        <Button type={"primary"}>add coulmen</Button>
+        <button></button>
+      </div>
+    </StyledHeader>
   );
 }
