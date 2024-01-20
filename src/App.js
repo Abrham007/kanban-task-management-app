67422d70b735eb73c122593ec3e31951dfd7e8c7
Abrham007 <abrhamaraya111@gmail.com>
2024-01-20 16:30:18 +0300

    added the theme managment boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import React, { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./theme/GlobalStyles";
+import { useTheme } from "./theme/useTheme";
 export default function App() {
+  const { theme, themeLoaded } = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+  }, [themeLoaded]);
   return (
     <>
-      <menu></menu>
-      <main></main>
+      {themeLoaded && (
+        <ThemeProvider theme={selectedTheme}>
+          <GlobalStyles></GlobalStyles>
+          <Header></Header>
+          <menu></menu>
+          <main></main>
+        </ThemeProvider>
+      )}
     </>
   );
 }
