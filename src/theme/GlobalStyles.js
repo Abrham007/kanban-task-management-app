935313ad18e10866cfba3c4520ba9ae68c654b0f
Abrham007 <abrhamaraya111@gmail.com>
2024-01-22 12:19:26 +0300

    added the Header theme

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -1,23 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 import { SecondaryButton } from "../components/Button";
 import { Logo } from "../components/Header";
+import { StyledHeader } from "../components/Header";
 
 export const GlobalStyles = createGlobalStyle`
   body { 
     font-family: 'Plus Jakarta Sans', sans-serif;
     transition: all 0.50s linear;
   }
+  ${StyledHeader} {
+    background-color: ${({ theme }) => theme.colors.header.background};
+    color: ${({ theme }) => theme.colors.header.text}
+  }
   ${Logo} {
-    background-image:url(${({ theme }) => require(`../${theme.images.logo}`)})
-      }
+    background-image: url(${({ theme }) => require(`../${theme.images.logo}`)})
+    }
   
   ${SecondaryButton} {
     background-color: ${({ theme }) => theme.colors.secondaryButton.background};
     color: ${({ theme }) => theme.colors.secondaryButton.text};
 
     &:hover {
-      background-color: ${({ theme }) =>
-        theme.colors.secondaryButton.hover.background};
+      background-color: ${({ theme }) => theme.colors.secondaryButton.hover.background};
     }
   }
   button:hover {
