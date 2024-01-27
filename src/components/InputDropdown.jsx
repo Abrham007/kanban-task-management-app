27cc9b80e49684cf6e8de656a145c794370915d2
Abrham007 <abrhamaraya111@gmail.com>
2024-01-27 10:00:38 +0300

    finished the InputDropdown components style with theme

---
+++ src/components/InputDropdown.jsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import downIcon from "../assets/icon-chevron-down.svg";
+import upIcon from "../assets/icon-chevron-up.svg";
+
+export const Dropdown = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+`;
+
+const DropdownInput = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 16px;
+  border-radius: 4px;
+  border: 1px solid rgba(130, 143, 163, 0.25);
+
+  &:hover {
+    cursor: pointer;
+    border: 1px solid #635fc7;
+  }
+
+  span {
+    font-size: 0.8125rem;
+    font-weight: 500;
+    line-height: 1.4375rem;
+  }
+`;
+
+const DropdownMenu = styled.menu`
+  position: absolute;
+  top: 47px;
+  left: 0;
+  width: 100%;
+  display: flex;
+  gap: 8px;
+  flex-direction: column;
+  padding: 16px;
+  border-radius: 8px;
+  box-shadow: 0px 10px 20px 0px rgba(54, 78, 126, 0.25);
+
+  li {
+    width: 100%;
+    list-style: none;
+  }
+
+  button {
+    width: 50%;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    color: #828fa3;
+    font-size: 0.8125rem;
+    font-weight: 500;
+    line-height: 1.4375rem;
+    text-align: left;
+
+    &:hover {
+      color: #000112;
+    }
+  }
+`;
+
+export default function InputDropdown() {
+  return (
+    <Dropdown aria-live="polite">
+      <DropdownInput>
+        <span>Doing</span>
+        <img src={downIcon} alt=""></img>
+      </DropdownInput>
+      <DropdownMenu>
+        <li>
+          <button>Todo</button>
+        </li>
+        <li>
+          <button>Doing</button>
+        </li>
+        <li>
+          <button>Done</button>
+        </li>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
