4d92c198be1e67608ff2f5077645080f27197a53
Abrham007 <abrhamaraya111@gmail.com>
2024-01-29 16:16:39 +0300

    made the component statfull

--- src/components/BoardMenu.jsx
+++ src/components/BoardMenu.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { devices } from "../utils/devices";
+import IconBoard from "./IconBoard";
+import { useContext } from "react";
+import { DataContext } from "../MyContext";
 
 const StyledBoardMenu = styled.div`
   width: 276px;
@@ -77,39 +80,23 @@ const CreateBtn = styled(MenuBtn)`
 `;
 
 export default function BoardMenu() {
+  const boardArray = useContext(DataContext);
+  const boardNameList = boardArray.map((board) => board.name);
   return (
     <StyledBoardMenu>
-      <h2>ALL BOARDS (3)</h2>
+      <h2>ALL BOARDS ({boardArray.length})</h2>
       <Menu>
-        <li>
-          <MenuBtn>
-            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#828FA3">
-              <path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" />
-            </svg>
-            <span>Platform Launch</span>
-          </MenuBtn>
-        </li>
-        <li>
-          <MenuBtn>
-            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#828FA3">
-              <path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" />
-            </svg>
-            <span>Marketing Plan</span>
-          </MenuBtn>
-        </li>
-        <li>
-          <MenuBtn>
-            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#828FA3">
-              <path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" />
-            </svg>
-            <span>Roadmap</span>
-          </MenuBtn>
-        </li>
+        {boardNameList.map((name) => (
+          <li key={name}>
+            <MenuBtn>
+              <IconBoard></IconBoard>
+              <span>{name}</span>
+            </MenuBtn>
+          </li>
+        ))}
         <li>
           <CreateBtn>
-            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="#635fc7">
-              <path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" />
-            </svg>
+            <IconBoard fill="#635FC7"></IconBoard>
             <span>+ Create New Board</span>
           </CreateBtn>
         </li>
