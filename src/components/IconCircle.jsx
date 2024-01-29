90c550cc6b11154c059a1f08a7a0da3d90440382
Abrham007 <abrhamaraya111@gmail.com>
2024-01-29 16:16:56 +0300

    created svg components for reusabilty

---
+++ src/components/IconCircle.jsx
@@ -0,0 +1,7 @@
+export default function IconCircle({ fill = "#49C4E5" }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill={fill}>
+      <circle cx="7.5" cy="7.5" r="7.5" />
+    </svg>
+  );
+}
