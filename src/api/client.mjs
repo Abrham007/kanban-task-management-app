a5bc5fa48e7ee43abb377a541d01b20e9ba6359f
Abrham007 <abrhamaraya111@gmail.com>
2024-02-05 14:55:50 +0300

    added the get and post for the project's table

--- src/api/client.mjs
+++ src/api/client.mjs
@@ -1,9 +1,17 @@
-export function postNewProject(newProject) {
-  return fetch("http://localhost:4000/project", {
+export async function postProjectAndColumn(newProject) {
+  const response = await fetch("http://localhost:4000/project", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newProject),
   });
+  const newProjectJson = await response.json();
+  return newProjectJson;
+}
+
+export async function fetchProjectAndColumnArray() {
+  const response = await fetch("http://localhost:4000/project");
+  const projectAndColumnArray = await response.json();
+  return projectAndColumnArray;
 }
--- src/api/server.mjs
+++ src/api/server.mjs
@@ -25,9 +25,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.post("/project", (req, res) => {
-  console.log("recieved by the back");
-  console.log(req.body);
+app.get("/project", async (req, res) => {
+  console.log("server reached");
+  let projectArray = (await db.query("SELECT * FROM project")).rows;
+  let columnArray = (await db.query("SELECT * FROM project_column")).rows;
+
+  console.log([projectArray, columnArray]);
+  res.json([projectArray, columnArray]);
+});
+
+app.post("/project", async (req, res) => {
+  console.log("sever reached");
+  let newProject;
+  let newColumns = [];
+  try {
+    let newProjectResponse = await db.query("INSERT INTO project (name) VALUES ($1) RETURNING *", [
+      req.body.projectName,
+    ]);
+    newProject = newProjectResponse.rows[0];
+    try {
+      Object.values(req.body.columnNames).forEach(async (columnName) => {
+        let newColumnResponse = await db.query(
+          "INSERT INTO project_column (name, project_id) VALUES ($1, $2) RETURNING *",
+          [columnName, newProject.id]
+        );
+        newColumns.push(newColumnResponse.rows[0]);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  res.json([newProject, newColumns]);
 });
 
 app.listen(port, () => {
