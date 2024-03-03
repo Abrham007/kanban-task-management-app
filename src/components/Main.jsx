26f86783646675d5d4c95e0998ee8a8bfaab37fa
Abrham007 <abrhamaraya111@gmail.com>
2024-03-03 15:01:36 +0300

    corrected the backdrop bug and improved the responsiveness

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,8 +9,10 @@ import TaskBoard from "./TaskBoard";
 export const StyledMain = styled.main`
   grid-area: main;
   display: flex;
-  align-items: ${({ $isMainEmpty }) => ($isMainEmpty ? "center" : "flex-start")};
-  justify-content: ${({ $isMainEmpty }) => ($isMainEmpty ? "center" : "flex-start")};
+  align-items: ${({ $isMainEmpty }) =>
+    $isMainEmpty ? "center" : "flex-start"};
+  justify-content: ${({ $isMainEmpty }) =>
+    $isMainEmpty ? "center" : "flex-start"};
   padding: 24px;
   transition: all 0.5s linear;
   overflow: auto;
@@ -19,12 +21,13 @@ export const StyledMain = styled.main`
     &::after {
       content: "";
       position: absolute;
-      top: 0;
+      top: 64px;
       left: 0;
       height: 100vh;
       width: 100vw;
       background-color: rgba(0, 0, 0, 0.4);
       opacity: 0;
+      z-index: -1;
       transition: opacity 0.5s linear;
     }
     ${({ $isSideBarHidden }) => {
@@ -32,6 +35,7 @@ export const StyledMain = styled.main`
         return css`
           &::after {
             opacity: 1;
+            z-index: 0;
           }
         `;
       }
@@ -41,20 +45,28 @@ export const StyledMain = styled.main`
 
 export default function Main() {
   const { isSideBarHidden } = useContext(SideBarContext);
-  const { projectArray, columnArray, selectedProjectId } = useContext(DataContext);
+  const { projectArray, columnArray, selectedProjectId } =
+    useContext(DataContext);
   const isEmptyBoard = projectArray.length === 0;
   let isEmptyColumn = true;
   let activeBoard;
 
   if (!isEmptyBoard) {
-    activeBoard = projectArray.find((project) => project.id === selectedProjectId);
-    const selectedColumn = columnArray.filter((col) => col.project_id === activeBoard.id);
+    activeBoard = projectArray.find(
+      (project) => project.id === selectedProjectId
+    );
+    const selectedColumn = columnArray.filter(
+      (col) => col.project_id === activeBoard.id
+    );
     isEmptyColumn = !selectedColumn || selectedColumn.length === 0;
   }
   return (
     <StyledMain $isSideBarHidden={isSideBarHidden} $isMainEmpty={isEmptyColumn}>
       {isEmptyBoard || isEmptyColumn ? (
-        <MainEmpty isEmptyBoard={isEmptyBoard} isEmptyColumn={isEmptyColumn}></MainEmpty>
+        <MainEmpty
+          isEmptyBoard={isEmptyBoard}
+          isEmptyColumn={isEmptyColumn}
+        ></MainEmpty>
       ) : (
         <TaskBoard></TaskBoard>
       )}
