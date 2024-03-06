c91a77ee682a8924be0c04bf9f83a5fbbd1fe8d8
Abrham007 <abrhamaraya111@gmail.com>
2024-03-06 16:09:34 +0300

    fixed the database connection bug

--- server/postgre.js
+++ server/postgre.js
@@ -1,9 +1,9 @@
 import pg from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+let conString = process.env.CONNECTION_STRING;
 export const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "tasks",
-  password: "a1b2r3h4",
-  port: 5432,
+  connectionString: conString,
 });
